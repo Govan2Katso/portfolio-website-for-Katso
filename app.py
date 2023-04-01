a297b7ef03e6a0f952fa4c558e1f309e29008f44
Govan2Katso <115703155+Govan2Katso@users.noreply.github.com>
2023-04-01 12:08:52 +0200

    more details

--- app.py
+++ app.py
@@ -1,9 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+JOBS = [
+    {
+    'id':1,
+    'title':'Web Developer',
+    'location':'Kuruman'
+
+    },
+    {
+    'id':2,
+    'title':'Penetration Tester',
+    'location':'Remote'
+    },
+    {
+    'id':3,
+    'title':'Software Developer',
+    'location':'Kathu'
+    },
+    {
+    'id':4,
+    'title':'Security Analyst',
+    'location':'Cape Town' 
+    }
+
+
+]
+
 @app.route('/')
-def home():
-    return "Home sweet home!"
+def job_opennings():
+    return render_template('home.html', jobs=JOBS, company_name='Katso')
+
+
 
 
 if __name__ == '__main__':
